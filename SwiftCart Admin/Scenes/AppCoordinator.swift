28b881005a0cbfefee5b6638993af5b52f140410
Ahmed El-Harby <CS.AhmedOsman@gmail.com>
2024-06-05 14:15:56 +0300

    Design Admin Home Screen

--- SwiftCart Admin/Scenes/AppCoordinator.swift
+++ SwiftCart Admin/Scenes/AppCoordinator.swift
@@ -17,15 +17,15 @@ class AppCoordinator: Coordinator {
     }
     
     func start() {
-//        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-//        let vc = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
-//        vc.coordinator = self
-//        navigationController.pushViewController(vc, animated: false)
-//    }
-//    
-//    func gotoAuth() {
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         let vc = storyboard.instantiateViewController(withIdentifier: "AuthViewController") as! AuthViewController
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+    
+    func gotoHome() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let vc = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
         vc.coordinator = self
         navigationController.pushViewController(vc, animated: true)
     }
@@ -34,3 +34,33 @@ class AppCoordinator: Coordinator {
         navigationController.popViewController(animated: true)
     }
 }
+
+
+//class AuthCoordinator: Coordinator {
+//
+//    var childCoordinators = [Coordinator]()
+//    var navigationController: UINavigationController
+//
+//    init(navigationController: UINavigationController) {
+//        self.navigationController = navigationController
+//    }
+//
+//    func start() {
+////        let vc = AuthViewControllers.instantiate()
+////        vc.coordinator = self
+////        navigationController.pushViewController(vc, animated: false)
+//    }
+//
+//    func finish() {
+//        navigationController.popViewController(animated: true)
+//    }
+//
+////    func gotoAnother() {
+////        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+////        let vc = storyboard.instantiateViewController(withIdentifier: "tab")
+////
+////        vc.coordinator = self
+////        navigationController.pushViewController(vc, animated: true)
+////    }
+//
+//}
--- SwiftCart Admin/Scenes/Home/HomeCoordinator.swift
+++
@@ -1,6 +0,0 @@
-//
-//  HomeCoor.swift
-//  SwiftCart Admin
-//
-//  Created by Mac on 03/06/2024.
-//
