f0eb90d37ded4f971beed1a9795d0272be5212f1
Ahmed El-Harby <CS.AhmedOsman@gmail.com>
2024-06-25 23:13:17 +0300

    Solve Some Isues

--- SwiftCart AdminTests/TryTests.swift
+++ SwiftCart AdminTests/TryTests.swift
@@ -26,4 +26,49 @@ class TryTests: XCTestCase {
         super.tearDown()
     }
     
+    func testMakeAPICall_Success_WithAllItemData(){
+        // Arrange
+        let expectation = self.expectation(description: "API call succeeds")
+        let priceRuleId: Int64 = 1099056087087
+        
+        // Act
+        let result =  service.makeAPICall(method: .get, endpoint: K.Endpoint.Coupons.discountCodes, byId: priceRuleId, itemData: nil)
+        
+        // Assert
+        result.subscribe{ data in
+            let discount: DiscountCodeResponse? = Utils.decode(from: data)
+            XCTAssertGreaterThan(discount?.discountCodes?.count ?? 0, 0)
+            expectation.fulfill()
+        } onError: { error in
+            XCTFail("Expected successful response, but got error: \(error)")
+        }.disposed(by: disposeBag)
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+
+    func testMakeAPICall_Success_WithAddItemData(){
+        // Arrange
+        let expectation = self.expectation(description: "API call succeeds")
+        let priceRuleId: Int64 = 1099056087087
+        let request = DiscountCodeResponse(discountCode: DiscountCode(code: "Test\(Int.random(in: 1000000..<10000000))"))
+        let jsonData = Utils.encode(request)
+        
+        // Act
+        let result =  service.makeAPICall(method: .post, endpoint: K.Endpoint.Coupons.discountCodes, byId: priceRuleId, itemData: jsonData)
+        
+        // Assert
+        result.subscribe{ data in
+            let discount: DiscountCodeResponse? = Utils.decode(from: data)
+            XCTAssertEqual(discount?.discountCode?.code, request.discountCode?.code)
+            expectation.fulfill()
+        } onError: { error in
+            print("Error: \(error.localizedDescription)")
+            XCTFail("Expected successful response, but got error: \(error)")
+        }.disposed(by: disposeBag)
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    
+
 }
